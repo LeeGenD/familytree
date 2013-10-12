59435a2e6c5efad552471ab879f027ebe9a68f2f
quanli <120757311@qq.com>
2013-10-12 08:27:51 +0800

    first commit

--- familytree.js
+++ familytree.js
@@ -5,13 +5,8 @@
 (function(window,document){
 
 var _familytree = {},
-_frameImg,
 some1
 ;
-
-_frameImg = document.createElement("img");
-_frameImg.src = "./img/frame.png";
-document.body.appendChild(_frameImg);
 
 _familytree.fn = function(){
 	//save data
@@ -37,7 +32,7 @@ _familytree.main.member = function(){
 	var member_stack = [];
 	var member_relation = [];
 	
-	function addMember(info,opt){
+	function addMember(info,opt,id){
 		var options = {
 			type:"normal",//random
 			minX:0,
@@ -50,6 +45,7 @@ _familytree.main.member = function(){
 			"sex":	"unknow",
 			"age":	"unknow",
 			"img":	"",
+			"id":	id,
 			"data":	{
 				posX:500,
 				posY:200,
@@ -70,13 +66,28 @@ _familytree.main.member = function(){
 		member_stack.push(information);
 	}
 	
+	function inRelation( relation){
+		for( _i = 0; _i < member_relation.length; _i++){
+			var r = member_relation[ _i];
+			if( relation.m1 == r.m1 && relation.m2 == r.m2){
+				return true;
+			}
+			if( relation.m1 == r.m2 && relation.m2 == r.m1){
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	function addRelation( m1, m2, description ){
 		var relation = {
 			"m1":	m1,
 			"m2":	m2,
 			"description":	description,
 		}
-		member_relation.push( relation );
+		if( ! inRelation( relation) ){
+			member_relation.push( relation );
+		}
 	}
 	
 	function setMemberPos(member,x,y){
@@ -110,6 +121,7 @@ _familytree.main.member = function(){
 _familytree.main.cvs = function(dom){
 	var cvs,
 	ctx,
+	_frameImg,
 	_width,
 	_height,
 	_moveX = 0, _moveY = 0,
@@ -127,6 +139,7 @@ _familytree.main.cvs = function(dom){
 		cvs = dom, ctx = dom.getContext("2d"), _width = dom.getAttribute("width"), _height = dom.getAttribute("height");
 		
 		ctx.strokeStyle="#dadada";
+		initImg();
 		/*
 		cvs.addEventListener("mousemove",shift.move);
 		cvs.addEventListener("mousedown",shift.start);
@@ -134,13 +147,71 @@ _familytree.main.cvs = function(dom){
 		*/
 		shift.bind(document.body);
 		zoom.bind(document.body);
+		client.bind(cvs);
 	}
 	
-	function _shift(event){
-		var x = event.clientX,
-		y = event.clientY;
-		_moveX = -x+500, _moveY = -y+500;
+	function initImg(){
+		_frameImg = document.createElement("img");
+		_frameImg.src = "./img/frame.png";
+		document.body.appendChild(_frameImg);
+		
+		_this._infoTop = document.createElement("img");
+		_this._infoTop.src = "./img/head.png"
+		document.body.appendChild(_this._infoTop);
+		
+		_this._infoMid = document.createElement("img");
+		_this._infoMid.src = "./img/mid.png"
+		document.body.appendChild(_this._infoMid);
+		
+		_this._infoBottom = document.createElement("img");
+		_this._infoBottom.src = "./img/footer.png"
+		document.body.appendChild(_this._infoBottom);
 	}
+	
+	var client = (function(){
+		var _clientX = 0, _clientY = 0;
+		
+		function setClient(event){
+			_clientX = event.clientX;
+			_clientY = event.clientY;
+		}
+		
+		function getClient(){
+			return {
+				"x":	_clientX,
+				"y":	_clientY,
+			}
+		}
+		
+		function getArea(){
+			var x = _clientX / _zoomPer - _moveX, y = _clientY / _zoomPer - _moveY;
+			
+			return {
+				"minX":	x - 180,
+				"maxX":	x,
+				"minY":	y - 207,
+				"maxY":	y,
+			}
+		}
+		
+		function inArea( x, y){
+			var area = getArea();
+			if( x > area.minX && x < area.maxX && y > area.minY && y < area.maxY ){
+				return true;
+			}
+		}
+		
+		function bind(dom){
+			dom.addEventListener("mousemove",setClient);
+		}		
+		
+		return {
+			"bind":	bind,
+			"getClient":	getClient,
+			"getArea":	getArea,
+			"inArea":	inArea,
+		}
+	})()
 	
 	var shift = (function(){
 		var _startX = 0,
@@ -229,8 +300,11 @@ _familytree.main.cvs = function(dom){
 		
 		function reduce(event){
 			_zoomPercentage -= 0.1;
-			if( _zoomPercentage <= 0.1 ){
-				_zoomPercentage = 0.1;
+			
+			var minPer = 0.3;
+			
+			if( _zoomPercentage <= minPer ){
+				_zoomPercentage = minPer;
 			}
 		}
 		
@@ -249,6 +323,7 @@ _familytree.main.cvs = function(dom){
 		
 		function bind(dom){
 			dom.addEventListener( "DOMMouseScroll", zoom);
+			dom.addEventListener( "mousewheel", zoom);
 		}
 		
 		return {
@@ -271,10 +346,35 @@ _familytree.main.cvs = function(dom){
 		
 		width = typeof width == Number ? width : 5; 
 		ctx.beginPath();
-	    ctx.lineWidth=width;
+	    ctx.lineWidth=width*_zoomPer;
 	    ctx.moveTo(x1,y1);
 	    ctx.lineTo(x2,y2);
 	    ctx.stroke();
+	}
+	
+	function drawMemberInfo(member){
+		var myClient = client.getClient(),
+		/*
+		x = member.data.posX + _moveX + 100,
+		y = member.data.posY + _moveY + 100;
+		x *= _zoomPer, y *= _zoomPer;
+		*/
+		x = myClient.x, y = myClient.y;
+		
+		ctx.drawImage( _this._infoTop, x, y, 228, 18);
+		
+		ctx.drawImage( _this._infoMid, x, y+18, 229, 15);
+		var top_offset = 28;
+		ctx.font = "15px AxureHandwritingRegular";
+		for(var o in member){
+			if( o != "data" && (typeof member[o] == "string" || typeof member[o] == "number")){
+				ctx.drawImage( _this._infoMid, x, y+top_offset, 229, 15);
+				ctx.fillText( o + ":" + member[o], x+15, y+top_offset);
+				top_offset += 15;
+			}
+		}
+		//ctx.drawImage( _this._infoMid, x, y+18, 229, 5);
+		ctx.drawImage( _this._infoBottom, x, y+top_offset, 229, 23);
 	}
 	
 	function drawAllLine(){
@@ -285,7 +385,7 @@ _familytree.main.cvs = function(dom){
 		x += _moveX, y += _moveY;
 		x *= _zoomPer, y *= _zoomPer;
 		
-		drawHead(head_img,x+22,y+12);
+		drawHead(head_img,x+22*_zoomPer,y+12*_zoomPer);
 		drawPhotoFrame(x,y);
 	}
 	
@@ -294,14 +394,25 @@ _familytree.main.cvs = function(dom){
 		ctx.clearRect( 0, 0, _width, _height);
 	}
 	
+	function returnMove(){
+		return {
+			moveX:	_moveX,
+			moveY:	_moveY,
+		}
+	}
+	
 	init(dom);
 	
 	return {
 		"drawLine":	drawLine,
 		"drawHead":	drawHead,
 		"drawMember":	drawMember,
+		"drawMemberInfo":	drawMemberInfo,
 		"clear":	clear,
 		"setBoundary"	:shift.setBoundary,
+		"getArea":	client.getArea,
+		"inArea":	client.inArea,
+		"returnMove":	returnMove,
 	}
 	
 }
@@ -315,6 +426,7 @@ _familytree.main.force_directed = (function(){
 	}
 	
 	//gravitation:the farther the bigger
+	/*
 	function getGravitation(x1,y1,x2,y2,min_distance){
 		var distance = getDistance(x1,y1,x2,y2);
 		//alert("g_"+distance);
@@ -336,9 +448,43 @@ _familytree.main.force_directed = (function(){
 			x2: distance * factor.x,
 			y2: distance * factor.y,
 		}
-	}	
+	}
+	*/
+	function getGravitation(x1,y1,x2,y2,remote){
+		var distance = getDistance(x1,y1,x2,y2);
+		if( distance < remote){
+			return {
+				x1: 0,
+				y1: 0,
+				x2: 0,
+				y2: 0,
+			}
+		}
+		var value = {
+			"left":	0,
+			"top":	0,
+		}
+		var speed=(distance/remote-1)*10;
+		speed/=distance;
+		value.left=speed*(x1-x2);
+		value.top=speed*(y1-y2);
+		var x = {
+			x1: - value.left,
+			y1: - value.top,
+			x2: value.left,
+			y2: value.top,
+		};
+		//alert( JSON.stringify(x) );
+		return {
+			x1: - value.left,
+			y1: - value.top,
+			x2: value.left,
+			y2: value.top,
+		}
+	}
 	
 	//Repulsion:the closer the bigger
+	/*
 	function getRepulsion(x1,y1,x2,y2,dis){
 		var distance = getDistance(x1,y1,x2,y2);
 		var factor = {
@@ -355,6 +501,75 @@ _familytree.main.force_directed = (function(){
 			y2: - distance * factor.y,
 		}
 	}
+	*/
+	
+	function Random( x, y){
+		return x + Math.random() * ( y - x );
+	}
+	
+	function getRepulsion(x1,y1,x2,y2,remote){
+		var value = {
+			"left":	0,
+			"top":	0,
+		}
+		var r=1;
+		//如果两点重合，让点获得随机的加速度
+		if(x1==x2&&y1==y2){
+			var sign=Random(0,4);
+			if(sign<1){
+				value.left=Random(0,r);
+				value.top=Random(0,r);
+			}
+			else if(sign<2){
+				value.left=Random(0,r);
+				value.top=-Random(0,r)
+			}
+			else if(sign<3){
+				value.left=-Random(0,r);
+				value.top=Random(0,r);
+			}
+			else{
+				value.left=-Random(0,r);
+				value.top=-Random(0,r);
+			}
+			/*
+			alert( JSON.stringify({x1: - value.left,
+				y1: - value.top,
+				x2: value.left,
+				y2: value.top,}));
+			*/
+			return {
+				x1: value.left,
+				y1: value.top,
+				x2: - value.left,
+				y2: - value.top,
+			}
+		}
+
+		//如果两点有距离
+		var distance = getDistance(x1,y1,x2,y2);
+		var speed=(1-distance/remote)*10;
+		//如果距离太大则没有加速度
+		if(speed<0){
+			return {
+				x1: 0,
+				y1: 0,
+				x2: 0,
+				y2: 0,
+			}
+		}
+		speed/=distance;
+		value.left=speed*(x1-x2);
+		value.top=speed*(y1-y2);
+		return {
+			x1: value.left,
+			y1: value.top,
+			x2: - value.left,
+			y2: - value.top,
+		}
+		return value;
+	}
+	
 	
 	return {
 		"getGravitation":	getGravitation,
@@ -412,7 +627,8 @@ window.familytree = function(canvas,opts,member_data,relations){
 			y1:	0,
 			x2:	0,
 			y2:	0,
-		}
+		},
+		fac = 5;
 		
 		//所有元素之间都有斥力
 		for( var _i = 0; _i < member_len ; _i++ ){
@@ -426,15 +642,15 @@ window.familytree = function(canvas,opts,member_data,relations){
 				y2 = m2.data.posY;
 			
 			
-				var repulsion = _familytree.main.force_directed.getRepulsion(x1,y1,x2,y2,1000);
+				var repulsion = _familytree.main.force_directed.getRepulsion(x1,y1,x2,y2,900);
 			
 				//var alert_div = document.getElementById("alert");
 				//alert_div.innerHTML = JSON.stringify(gravitation)+JSON.stringify(repulsion);
 				//repulsion = zero;
 				//gravitation = zero;
 			
-				_member.setMemberPos( m1, x1+(repulsion.x1*0.01), y1+(repulsion.y1*0.01));
-				_member.setMemberPos( m2, x2+(repulsion.x2*0.01), y2+(repulsion.y2*0.01));
+				_member.setMemberPos( m1, x1+(repulsion.x1*fac), y1+(repulsion.y1*fac));
+				_member.setMemberPos( m2, x2+(repulsion.x2*fac), y2+(repulsion.y2*fac));
 			}
 			
 			/*
@@ -447,7 +663,7 @@ window.familytree = function(canvas,opts,member_data,relations){
 				continue;
 			}
 			var repulsion = _familytree.main.force_directed.getRepulsion(x1,y1,first_member.data.posX,first_member.data.posY,1000);
-			_member.setMemberPos( m1, x1+(repulsion.x1*0.01), y1+(repulsion.y1*0.01));
+			_member.setMemberPos( m1, x1+(repulsion.x1*fac), y1+(repulsion.y1*fac));
 		}
 		
 		//只有有关系元素的有引力
@@ -460,21 +676,24 @@ window.familytree = function(canvas,opts,member_data,relations){
 			x2 = m2.data.posX,
 			y2 = m2.data.posY;
 			
-			var gravitation = _familytree.main.force_directed.getGravitation(x1,y1,x2,y2,100),
-			repulsion = _familytree.main.force_directed.getRepulsion(x1,y1,x2,y2,300);
+			var gravitation = _familytree.main.force_directed.getGravitation(x1,y1,x2,y2,200),
+			repulsion = _familytree.main.force_directed.getRepulsion(x1,y1,x2,y2,3000);
 			
-			var alert_div = document.getElementById("alert");
-			alert_div.innerHTML = JSON.stringify(gravitation)+JSON.stringify(repulsion);
 			//repulsion = zero;
 			//gravitation = zero;
 			
-			_member.setMemberPos( m1, x1+(gravitation.x1*0.01), y1+(gravitation.y1*0.01));
-			_member.setMemberPos( m2, x2+(gravitation.x2*0.01), y2+(gravitation.y2*0.01));
+			_member.setMemberPos( m1, x1+(gravitation.x1*fac), y1+(gravitation.y1*fac));
+			_member.setMemberPos( m2, x2+(gravitation.x2*fac), y2+(gravitation.y2*fac));
 		}
 		
 		//设置边界（需优化）
 		var minX = minY = 9999,
 		maxX = maxY = -9999;
+		
+		var pos = _canvas.getArea();
+		var alert_div = document.getElementById("alert");
+		//alert_div.innerHTML = JSON.stringify(JSON.stringify(pos));
+		//alert_div.innerHTML = JSON.stringify(JSON.stringify(pos)+JSON.stringify(member[0].data));
 		for( var _i = 0; _i < member_len ; _i++ ){
 			var m = member[ _i],
 			x = m.data.posX,
@@ -491,8 +710,13 @@ window.familytree = function(canvas,opts,member_data,relations){
 			if( y > maxY ){
 				maxY = y;
 			}
+			if( _canvas.inArea( x, y) ){
+				_canvas.drawMemberInfo(m);
+				//alert_div.innerHTML = JSON.stringify("!!!"+JSON.stringify(pos)+JSON.stringify(m.id));
+			}
 		}
-		_canvas.setBoundary( minX, maxX, minY, maxY);
+		//alert_div.innerHTML = JSON.stringify("minX:" + parseInt(minX) + ",maxX:" + parseInt(maxX) + ",minY:" + parseInt(minY) + ",maxY:" + parseInt(maxY) +JSON.stringify(_canvas.returnMove()));
+		//_canvas.setBoundary( minX, maxX, minY, maxY);//设置鼠标滑动边界
 		//alert( _canvas.setBoundary );
 	}
 	
@@ -502,6 +726,14 @@ window.familytree = function(canvas,opts,member_data,relations){
 	}
 	
 	function initData(){
+		_member.addMember( member_data[ 0 ] , {
+			"type":"random",
+			"minX":100,
+			"maxX":900,
+			"minY":100,
+			"maxY":300,
+		},0);
+		/*
 		for( var _i = 0; _i < member_data.length ; _i++ ){
 			_member.addMember( member_data[ _i ] , {
 				"type":"random",
@@ -516,10 +748,66 @@ window.familytree = function(canvas,opts,member_data,relations){
 		for( var _i = 0; _i < relations.length ; _i++ ){
 			_member.addRelation( member[ relations[_i][0] ], member[ relations[_i][1] ], "relation");
 		}
+		*/
 	}
+	function getRelations(x){
+		var relations_arr = [];
+		
+		for( var _i = 0; _i < relations.length ; _i++ ){
+			if( relations[ _i][0] == x ){
+				relations_arr.push(relations[ _i][1]);
+			}
+			if( relations[ _i][1] == x ){
+				relations_arr.push(relations[ _i][0]);
+			}
+		}
+		
+		return relations_arr;
+	}
+	function giveBranch(){
+		var member = _member.getAllMember();
+		//alert( JSON.stringify(member[0]) );
+		var member_len = member.length;
+		
+		for( var _i = 0; _i < member_len; _i++ ){
+			var rel_arr = getRelations(member[ _i].id);
+			
+			for( var _j = 0; _j < rel_arr.length; _j++){
+				//alert( _j );
+				
+				for( var _k = 0; _k < member.length; _k++){
+					//alert( member[ _k].id+","+rel_arr[ _j]);
+					if( member[ _k].id == rel_arr[ _j] ){
+						break;
+					}
+				}
+				
+				//alert( _k+",,,"+member.length );
+				if( _k == member.length){
+					if( rel_arr[ _j] == 7 ){
+						//continue;
+					}
+					_member.addMember( member_data[ rel_arr[ _j] ] , {
+						"type":"random",
+						"minX":100,
+						"maxX":900,
+						"minY":100,
+						"maxY":300,
+					},rel_arr[ _j]);
+					//alert( rel_arr[ _j] );
+					_member.addRelation( member[ _i], member[ member.length-1], "relation");
+				}
+				else{
+					_member.addRelation( member[ _i], member[ _k], "relation");
+				}
+				
+			}
+		}
+	}
+	document.getElementById("branch_btn").addEventListener("click",giveBranch);
 	initData();
 	
-	setInterval(Animation,10);
+	setInterval(Animation,20);
 	
 }
 
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ familytree(
 	
 	[
 		{
-			"name":	"Jessica Liu",
+			"name":	"Jessica Liu1",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("unique"),
@@ -13,47 +13,89 @@ familytree(
 				posY:200,
 				position:	"fixed",
 			}
-		},
+		},//0
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("tly"),
-		},
+		},//1
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("jessica"),
-		},
+		},//2
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("jessica"),
-		},
+		},//3
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("jessica"),
-		},
+		},//4
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("jessica"),
-		},
+		},//5
 		{
 			"name":	"Jessica Liu",
 			"sex":	"unknow",
 			"age":	"18",
 			"img":	document.getElementById("jessica"),
-		},
+		},//6
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("unique"),
+		},//7
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("tly"),
+		},//8
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("jessica"),
+		},//9
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("jessica"),
+		},//10
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("jessica"),
+		},//11
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("jessica"),
+		},//12
+		{
+			"name":	"Jessica Liu",
+			"sex":	"unknow",
+			"age":	"18",
+			"img":	document.getElementById("jessica"),
+		},//13
 	]
 	,
 	[
-		[0,1],[1,2],[0,3],[3,4],[1,5],[0,6]
+		[0,1],[1,2],[0,3],[3,4],[1,5],[0,6],[6,7],[6,8],[3,9],[9,10],[10,11],[10,12]
 	]
 );
 
